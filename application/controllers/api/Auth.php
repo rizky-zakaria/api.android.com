0c052a705f842ca0edceb48c51f02870b7e7eabd
rizkyzakaria_ <rizky.zakaria78@gmail.com>
2020-11-23 20:53:09 +0800

    first commit

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -88,15 +88,15 @@ class Auth extends REST_Controller
 
     public function index_post()
     {
-        $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
-            'is_unique' => 'This email has already registered!'
-        ]);
-        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
-            'matches' => 'Password dont match!',
-            'min_length' => 'Password too short!'
-        ]);
-        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+        // $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        // $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+        //     'is_unique' => 'This email has already registered!'
+        // ]);
+        // $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+        //     'matches' => 'Password dont match!',
+        //     'min_length' => 'Password too short!'
+        // ]);
+        // $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
 
         if ($this->form_validation->run() == false) {
             $data['title'] = 'WPU User Registration';
@@ -104,12 +104,12 @@ class Auth extends REST_Controller
             $this->load->view('auth/registration');
             $this->load->view('templates/auth_footer');
         } else {
-            $email = $this->input->post('email', true);
+            $email = $this->post('email');
             $data = [
-                'name' => htmlspecialchars($this->input->post('name', true)),
-                'email' => htmlspecialchars($email),
+                'name' => $this->post('name'),
+                'email' => $email,
                 'image' => 'default.jpg',
-                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'password' => $this->post('password'),
                 'role_id' => 2,
                 'is_active' => 0,
                 'date_created' => time()
