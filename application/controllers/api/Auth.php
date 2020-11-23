bde4f81cc77b28edb1d321c556faa709e112ead9
rizkyzakaria_ <rizky.zakaria78@gmail.com>
2020-11-23 20:39:11 +0800

    first commit

--- application/controllers/api/Auth.php
+++ application/controllers/api/Auth.php
@@ -16,15 +16,164 @@ class Auth extends REST_Controller
         $this->load->model('User_model', 'user');
     }
 
-
     public function index_get()
     {
-        $user = $this->user->getUser();
+        $username = $this->get('username');
+        $password = $this->get('password');
+
+        if ($username === null && $password === null) {
+            $user = $this->user->getUser();
+        } else {
+            $user = $this->user->getUser($username, $password);
+        }
+
         if ($user) {
             $this->response([
-                'status' => TRUE,
-                'data' => $user
+                'status' => true,
+                'data' => $user,
             ], REST_Controller::HTTP_OK);
+        } else {
+            $this->response([
+                'status' => false,
+                'messages' => "Data Tidak Di Temukan",
+            ], REST_Controller::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function index_delete()
+    {
+        $nik = $this->delete('nik');
+        if ($nik === null) {
+            $this->response([
+                'status' => false,
+                'messages' => "Pilih data terlebih dahulu",
+            ], REST_Controller::HTTP_BAD_REQUEST);
+        } else {
+            if ($this->user->delUser($nik) > 0) {
+                $this->response([
+                    'status' => true,
+                    'messages' => "Data Berhasil Di Hapus",
+                ], REST_Controller::HTTP_ACCEPTED);
+            } else {
+                $this->response([
+                    'status' => false,
+                    'messages' => "Data Gagal di Hapus",
+                ], REST_Controller::HTTP_CONFLICT);
+            }
+        }
+    }
+
+    public function index_put()
+    {
+        $old_nik = $this->put('old_nik');
+        $data = [
+            'nik' => $this->post('nik'),
+            'email' => $this->post('email'),
+            'username' => $this->post('username'),
+            'password' => $this->post('password'),
+        ];
+
+        if ($this->user->updateUser($data, $old_nik) > 0) {
+            $this->response([
+                'status' => true,
+                'messages' => "User Has Been Updated",
+            ], REST_Controller::HTTP_CREATED);
+        } else {
+            $this->response([
+                'status' => false,
+                'messages' => "Failed create user",
+            ], REST_Controller::HTTP_NOT_ACCEPTABLE);
+        }
+    }
+
+    public function index_post()
+    {
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'WPU User Registration';
+            $this->load->view('templates/auth_header', $data);
+            $this->load->view('auth/registration');
+            $this->load->view('templates/auth_footer');
+        } else {
+            $email = $this->input->post('email', true);
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($email),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 0,
+                'date_created' => time()
+            ];
+
+            // siapkan token
+            $token = base64_encode(random_bytes(32));
+            $user_token = [
+                'email' => $email,
+                'token' => $token,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->db->insert('user_token', $user_token);
+
+            $send = $this->_sendEmail($token, 'verify');
+
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Congratulation! your account has been created. Please activate your account</div>');
+            if ($send) {
+                $this->response([
+                    'status' => false,
+                    'messages' => "verify your email account",
+                ], REST_Controller::HTTP_OK);
+            } else {
+                $this->response([
+                    'status' => false,
+                    'messages' => "can't not send verify code",
+                ], REST_Controller::HTTP_NO_CONTENT);
+            }
+        }
+    }
+
+    private function _sendEmail($token, $type)
+    {
+        $config = [
+            'protocol'  => 'smtp',
+            'smtp_host' => 'ssl://smtp.googlemail.com',
+            'smtp_user' => 'emailtesting364@gmail.com',
+            'smtp_pass' => 'emailtesting20',
+            'smtp_port' => 465,
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8',
+            'newline'   => "\r\n"
+        ];
+
+        $this->email->initialize($config);
+
+        $this->email->from('emailtesting20@gmail.com', 'Aktifkan Akun Anda Sekarang');
+        $this->email->to($this->input->post('email'));
+
+        if ($type == 'verify') {
+            $this->email->subject('Account Verification');
+            $this->email->message('Click this link to verify you account : <a href="' . base_url() . 'auth/verify?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Activate</a>');
+        } else if ($type == 'forgot') {
+            $this->email->subject('Reset Password');
+            $this->email->message('Click this link to reset your password : <a href="' . base_url() . 'auth/resetpassword?email=' . $this->input->post('email') . '&token=' . urlencode($token) . '">Reset Password</a>');
+        }
+
+        if ($this->email->send()) {
+            return true;
+        } else {
+            echo $this->email->print_debugger();
+            die;
         }
     }
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -3,8 +3,52 @@
 class User_model extends CI_Model
 {
 
-    public function getUser()
+    public function getUser($username = null, $password = null)
     {
-        return $this->db->get('tb_user');
+        if ($username === null && $password === null) {
+            $query = "SELECT * FROM user";
+            return $this->db->query($query)->result_array();
+        } else {
+            return $this->db->get_where('user', ['username' => $username, 'password' => MD5($password)])->result_array();
+        }
+    }
+
+    public function delUser($id_user)
+    {
+        $this->db->delete('tb_user', ['id_user' => $id_user]);
+        return $this->db->affected_rows();
+    }
+
+    public function createUser($data)
+    {
+        $data = array(
+            'name' => 'My title',
+            'email'  => 'My Name',
+            'image'  => 'My date'
+        );
+
+        $sql = $this->db->set($data)->get_compiled_insert('mytable');
+        $this->db->insert('user', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function cekUser($username, $password)
+    {
+        // return $result = $this->db->get_where('tb_user', ['username' => $username, 'password' => MD5($password)])->result_array();
+        $result = $this->db->query("SELECT * FROM tb_user WHERE username='$username' AND password = MD5('$password') ")->result_array();
+        return $result;
+    }
+
+    public function updateUser($data, $old_nik)
+    {
+        $this->db->update('tb_user', $data, ['nik' => $old_nik]);
+        return $this->db->affected_rows();
+    }
+
+    public function cek($id_user)
+    {
+        // return $result = $this->db->get_where('tb_user', ['username' => $username, 'password' => MD5($password)])->result_array();
+        $result = $this->db->query("SELECT * FROM tb_pinjam WHERE id_user = '$id_user' ")->result_array();
+        return $result;
     }
 }
